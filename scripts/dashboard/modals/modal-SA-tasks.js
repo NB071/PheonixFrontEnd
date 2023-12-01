da56b1400b7dc6436e572b2d86779d689414a158
Nima Bargestan <nimam700@gmail.com>
2023-12-01 14:46:15 -0500

    Fix issue on see all modal and add sorting in see both modals

--- scripts/dashboard/modals/modal-SA-tasks.js
+++ scripts/dashboard/modals/modal-SA-tasks.js
@@ -8,7 +8,7 @@ function calcucalteDue({ start: startDate, to: dueDate }) {
 
   const newHour = dueHour - startHour;
   const newMinute = dueMinutes - startMinutes;
-console.log(dueHour, startHour);
+  console.log(dueHour, startHour);
   // If paramters are not valid
   if (newHour < 0 || newMinute < 0) return -1;
 
@@ -35,8 +35,8 @@ function mapPriority(priority) {
 
 export default function openSATasks(tasksArray) {
   const totalTasks = tasksArray.length;
-  const remainingTasks = tasksArray.filter(
-    (task) => !isTodoPassedDeadline("task", task.time_box.to)
+  const remainingTasks = tasksArray.filter((task) =>
+    isTodoPassedDeadline("task", task.time_box.to)
   );
   const [modal, modalBox] = createModal({
     modalClass: "SA-tasks",
@@ -84,88 +84,96 @@ export default function openSATasks(tasksArray) {
   // all tasks wrapper
   const allTasks = document.createElement("div");
   allTasks.classList.add("SA-tasks__cards-wrapper");
-  tasksArray.forEach(({ id, time_box, title, description, priority, done }) => {
-    const CPDueNumber = done ? "-" : calcucalteDue(time_box);
+  tasksArray
+    .sort((a, b) => {
+      const date = new Date();
+      const year = date.getFullYear();
+      const month = date.getMonth();
+      const day = date.getDate();
+      const dA = new Date(year, month, day, ...timeTo24(a.time_box.to));
+      const dB = new Date(year, month, day, ...timeTo24(b.time_box.to));
+      return dA - dB;
+    })
+    .forEach(({ id, time_box, title, description, priority, done }) => {
+      const CPDueNumber = done ? "-" : calcucalteDue(time_box);
 
-    // Card Element
-    const cardEl = document.createElement("article");
-    cardEl.classList.add("SA-tasks__task");
-    cardEl.setAttribute("key", `task-P${id}X`);
-    cardEl.addEventListener("click", () =>
-      openModifyTask({ id, title, priority, description, time_box })
-    );
+      // Card Element
+      const cardEl = document.createElement("article");
+      cardEl.classList.add("SA-tasks__task");
+      cardEl.setAttribute("key", `task-P${id}X`);
+      cardEl.addEventListener("click", () =>
+        openModifyTask({ id, title, priority, description, time_box })
+      );
 
-    // left
-    const cardInfo = document.createElement("div");
-    cardInfo.classList.add("SA-tasks__card-info");
+      // left
+      const cardInfo = document.createElement("div");
+      cardInfo.classList.add("SA-tasks__card-info");
 
-    // Icon
-    const cardIcon = document.createElement("i");
-    cardIcon.classList.add("SA-tasks__card-icon", "fa-solid", "fa-school");
+      // Icon
+      const cardIcon = document.createElement("i");
+      cardIcon.classList.add("SA-tasks__card-icon", "fa-solid", "fa-school");
 
-    // task info wrapper
-    const taskInfoWrapper = document.createElement("div");
-    taskInfoWrapper.classList.add("SA-tasks__task-info-wrapper");
+      // task info wrapper
+      const taskInfoWrapper = document.createElement("div");
+      taskInfoWrapper.classList.add("SA-tasks__task-info-wrapper");
 
-    // Task title/desc
-    const taskTitle = document.createElement("h2");
-    taskTitle.classList.add("SA-tasks__card-title");
-    taskTitle.textContent = `Task "${title}"`;
+      // Task title/desc
+      const taskTitle = document.createElement("h2");
+      taskTitle.classList.add("SA-tasks__card-title");
+      taskTitle.textContent = `Task "${title}"`;
 
-    const taskDescription = document.createElement("h2");
-    taskDescription.classList.add("SA-tasks__card-description");
-    taskDescription.textContent = `Description: ${description}`;
+      const taskDescription = document.createElement("h2");
+      taskDescription.classList.add("SA-tasks__card-description");
+      taskDescription.textContent = `Description: ${description}`;
 
-    taskInfoWrapper.append(taskTitle, taskDescription);
+      taskInfoWrapper.append(taskTitle, taskDescription);
 
-    cardInfo.append(cardIcon, taskInfoWrapper);
+      cardInfo.append(cardIcon, taskInfoWrapper);
 
-    // Right
-    const cardProgress = document.createElement("div");
-    cardProgress.classList.add("SA-tasks__card-progress");
+      // Right
+      const cardProgress = document.createElement("div");
+      cardProgress.classList.add("SA-tasks__card-progress");
 
-    // Due
-    const CPDue = document.createElement("p");
-    CPDue.classList.add("SA-tasks__card-due");
+      // Due
+      const CPDue = document.createElement("p");
+      CPDue.classList.add("SA-tasks__card-due");
 
-    // conditions for due output
-    const CPDueText = document.createElement("span");
-    CPDueText.classList.add("SA-tasks__card-due-text");
-    CPDueText.textContent = "Due: ";
+      // conditions for due output
+      const CPDueText = document.createElement("span");
+      CPDueText.classList.add("SA-tasks__card-due-text");
+      CPDueText.textContent = "Due: ";
 
-    CPDue.append(CPDueText, CPDueNumber);
+      CPDue.append(CPDueText, CPDueNumber);
 
-    // Done
-    const isDone = document.createElement("p");
-    isDone.classList.add("SA-tasks__card-done");
+      // Done
+      const isDone = document.createElement("p");
+      isDone.classList.add("SA-tasks__card-done");
 
-    // conditions for due output
-    const isDoneText = document.createElement("span");
-    isDoneText.classList.add("SA-tasks__card-done-text");
-    isDoneText.textContent = "Done: ";
+      // conditions for due output
+      const isDoneText = document.createElement("span");
+      isDoneText.classList.add("SA-tasks__card-done-text");
+      isDoneText.textContent = "Done: ";
 
-    const isDoneValue = document.createTextNode(
-      `${done ? "Yes" : "No"}`
-    );
+      const isDoneValue = document.createTextNode(`${done ? "Yes" : "No"}`);
 
-    isDone.append(isDoneText, isDoneValue);
+      isDone.append(isDoneText, isDoneValue);
 
-    // Priority
-    const priorityP = document.createElement("p");
-    priorityP.classList.add("SA-tasks__card-priority");
+      // Priority
+      const priorityP = document.createElement("p");
+      priorityP.classList.add("SA-tasks__card-priority");
 
-    // conditions for due output
-    const priorityText = document.createElement("span");
-    priorityText.classList.add("SA-tasks__card-priority-text");
-    priorityText.textContent = "Priority: ";
+      // conditions for due output
+      const priorityText = document.createElement("span");
+      priorityText.classList.add("SA-tasks__card-priority-text");
+      priorityText.textContent = "Priority: ";
 
-    const mappedPriority = mapPriority(priority);
-    priorityP.append(priorityText, mappedPriority);
+      const mappedPriority = mapPriority(priority);
+      priorityP.append(priorityText, mappedPriority);
 
-    cardProgress.append(CPDue, isDone, priorityP);
-    cardEl.append(cardInfo, cardProgress);
-    allTasks.appendChild(cardEl);
-  });
+      cardProgress.append(CPDue, isDone, priorityP);
+      cardEl.append(cardInfo, cardProgress);
+      allTasks.appendChild(cardEl);
+    });
 
   modalBox.append(taskStats, allTasks);
   modal.appendChild(modalBox);
